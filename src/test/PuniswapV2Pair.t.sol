d2c516c63d6a8323397a5a484db38f476f07a624
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-04-21 23:22:37 +0200

    Add new unit tests that cover the part of the Pair contract.

--- src/test/PuniswapV2Pair.t.sol
+++ src/test/PuniswapV2Pair.t.sol
@@ -40,6 +40,41 @@ contract PuniswapV2PairTest is DSTest {
         assertEq(reserve1, expectedReserve1, "unexpected reserve1");
     }
 
+    function assertCumulativePrices(
+        uint256 expectedPrice0,
+        uint256 expectedPrice1
+    ) internal {
+        assertEq(
+            pair.price0CumulativeLast(),
+            expectedPrice0,
+            "unexpected cumulative price 0"
+        );
+        assertEq(
+            pair.price1CumulativeLast(),
+            expectedPrice1,
+            "unexpected cumulative price 1"
+        );
+    }
+
+    function calculateCurrentPrice()
+    internal
+    returns (uint256 price0, uint256 price1)
+    {
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        price0 = reserve0 > 0
+        ? (reserve1 * uint256(UQ112x112.Q112)) / reserve0
+        : 0;
+        price1 = reserve1 > 0
+        ? (reserve0 * uint256(UQ112x112.Q112)) / reserve1
+        : 0;
+    }
+
+    function assertBlockTimestampLast(uint32 expected) internal {
+        (, , uint32 blockTimestampLast) = pair.getReserves();
+
+        assertEq(blockTimestampLast, expected, "unexpected blockTimestampLast");
+    }
+
     // Any function starting with "test" is a test case.
 
     function testMintBootstrap() public {
@@ -51,6 +86,41 @@ contract PuniswapV2PairTest is DSTest {
         assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
         assertReserves(1 ether, 1 ether);
         assertEq(pair.totalSupply(), 1 ether);
+    }
+
+    function testMintWhenTheresLiquidity() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+
+        vm.warp(37);
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 2 ether);
+
+        pair.mint(); // + 2 LP
+
+        assertEq(pair.balanceOf(address(this)), 3 ether - 1000);
+        assertEq(pair.totalSupply(), 3 ether);
+        assertReserves(3 ether, 3 ether);
+    }
+
+
+    function testMintLiquidityUnderflow() public {
+        // 0x11: If an arithmetic operation results in underflow or overflow outside of an unchecked { ... } block.
+        vm.expectRevert(
+            hex"4e487b710000000000000000000000000000000000000000000000000000000000000011"
+        );
+        pair.mint();
+    }
+
+    function testMintZeroLiquidity() public {
+        token0.transfer(address(pair), 1000);
+        token1.transfer(address(pair), 1000);
+
+        vm.expectRevert(hex"d226f9d4"); // InsufficientLiquidityMinted()
+        pair.mint();
     }
 
 }
